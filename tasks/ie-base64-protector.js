bb749ce06be64e786f42a3a11a10880b9de67608
Dean James <User@Deans-Pro.local>
2015-04-22 00:12:29 +0100

    Fixed require location.

--- tasks/ie-base64-protector.js
+++ tasks/ie-base64-protector.js
@@ -5,7 +5,7 @@ module.exports = function (grunt) {
 
     'use strict';
 
-    var protector = require('lib/protector.js');
+    var protector = require('./lib/protector');
 
     grunt.registerMultiTask('ieBase64Protector', 'Duplicates blocks of styles that contain a background image, adds a custom prefix to the selector and adds a comment after the background style to prevent imageEmbed from encoding the image', function () {
 
