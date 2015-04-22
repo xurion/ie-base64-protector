65aca5749bb513eb0b8748afed582ed0e172faae
Dean James <User@Deans-Pro.local>
2015-04-22 22:47:26 +0100

    Added file IO.

--- tasks/ie-base64-protector.js
+++ tasks/ie-base64-protector.js
@@ -5,7 +5,8 @@ module.exports = function (grunt) {
 
     'use strict';
 
-    var protector = require('./lib/protector');
+    var protector = require('./lib/protector'),
+        newStyles;
 
     grunt.registerMultiTask('ieBase64Protector', 'Duplicates blocks of styles that contain a background image, adds a custom prefix to the selector and adds a comment after the background style to prevent imageEmbed from encoding the image', function () {
 
@@ -17,8 +18,9 @@ module.exports = function (grunt) {
 
             if (cssFiles.hasOwnProperty(selectorPrefix)) {
 
-                protector.protect(cssFiles[selectorPrefix], selectorPrefix);
-
+                fs.readFileSync(cssFiles[selectorPrefix]).toString();
+                newStyles = protector.protect(cssFiles[selectorPrefix], selectorPrefix);
+                fs.appendFileSync(cssFiles[selectorPrefix], newStyles);
             }
         }
     });
