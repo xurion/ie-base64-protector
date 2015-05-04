dd4034a61e6e0b920faa9bfe79b6a26ef915033a
Dean James <User@Deans-Pro.local>
2015-05-04 14:04:41 +0100

    Added travis config. Removed JSLint.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,26 +11,16 @@ module.exports = function (grunt) {
                 jshintrc: '.jshintrc'
             }
         },
-        jslint: {
-            client: {
-                src: ['Gruntfile.js', 'tasks/*.js', 'test/*.js'],
-                directives: {
-                    browser: true
-                }
-            }
-        },
         nodeunit: {
             all: ['test/*.js']
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-jslint');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
     grunt.registerTask('test', [
         'jshint',
-        'jslint',
         'nodeunit'
     ]);
 
